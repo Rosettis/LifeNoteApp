f3907d9d5d72c72f2ac7e65c3251f18c76cbd33c
RosettM <matthew.rosettis@gmail.com>
2015-11-24 11:38:52 -0500

    refactored to change project name

--- app/src/main/java/capstone/uoit/ca/lifenoteapp/StartActivity.java
+++ app/src/main/java/capstone/uoit/ca/lifenoteapp/StartActivity.java
@@ -1,12 +1,12 @@
-package capstone.uoit.ca.mobileapp;
+package capstone.uoit.ca.lifenoteapp;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import capstone.uoit.ca.mobileapp.login.LoginActivity;
-import capstone.uoit.ca.mobileapp.signup.SignUpActivity;
+import capstone.uoit.ca.lifenoteapp.login.LoginActivity;
+import capstone.uoit.ca.lifenoteapp.signup.SignUpActivity;
 
 /** @author Matthew Rosettis */
 public class StartActivity extends AppCompatActivity {

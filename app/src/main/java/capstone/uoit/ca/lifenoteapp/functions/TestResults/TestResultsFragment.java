f25c9471a8698c9b8922b978e7a5ecfc7e8e8886
Matthew <matt_rose21@hotmail.com>
2016-03-23 02:02:55 -0400

    Test Results 60% done, need to put images into a layout that scrolls and has rows and columns. Also need to ensure that the images can be seen once they are chosen for upload (want to add them to the database and can be pulled from there).

--- app/src/main/java/capstone/uoit/ca/lifenoteapp/functions/TestResults/TestResultsFragment.java
+++ app/src/main/java/capstone/uoit/ca/lifenoteapp/functions/TestResults/TestResultsFragment.java
@@ -2,10 +2,41 @@ package capstone.uoit.ca.lifenoteapp.functions.TestResults;/**
  * Created by 100490515 on 11/16/2015.
  */
 
-
-import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import capstone.uoit.ca.lifenoteapp.R;
 
 /**
  * TestResultsFragment
@@ -13,9 +44,172 @@ import android.widget.ImageView;
  * @author Matthew Rosettis
  */
 public class TestResultsFragment extends Fragment {
+    View view;
     ImageView viewImage;
     Button b;
 
+
+    public static TestResultsFragment newInstance(){
+        TestResultsFragment fragment = new TestResultsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TestResultsFragment(){
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_test_results, container, false);
+        viewImage=(ImageView) view.findViewById(R.id.viewImage);
+        FloatingActionButton btnFab = (FloatingActionButton) view.findViewById(R.id.btnFloatingAction);
+        btnFab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Let's add an image", Toast.LENGTH_SHORT).show();
+                selectImage();
+            }
+        });
+
+        return view;
+    }
+
+    private void selectImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Add Photo");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    getActivity().startActivityForResult(intent, 1);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    getActivity().startActivityForResult(intent, 2);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("ResultCode",Integer.toString(resultCode));
+            if (requestCode == 1) {
+                /*File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    viewImage.setImageBitmap(bitmap);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "lifeNote" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }*/
+                try {
+                    Bundle extra = data.getExtras();
+                    Bitmap bitmap = (Bitmap) extra.get("data");
+                    viewImage.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContext().getContentResolver().query(selectedImage, filePath, null, null, null);
+                assert c != null;
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.d("pathOfImageFromGallery", picturePath+"");
+                viewImage.setImageBitmap(thumbnail);
+            }
+
+    }
+    /*//Attempting to save their photo to their phone and then display the Full-Sized photo
+    String photoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CANADA).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  *//* prefix *//*
+                ".jpg",         *//* suffix *//*
+                storageDir      *//* directory *//*
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        photoPath = "file:" + image.getAbsolutePath();
+        galleryAddPic();
+        return image;
+    }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(photoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+*/
+    public void displayTestResults() {
+        TestResultsFragment frag = TestResultsFragment.newInstance();
+        FragmentManager fragManager = getFragmentManager();
+
+        FragmentTransaction transaction = fragManager.beginTransaction();
+        transaction
+                .replace(R.id.content, frag)
+                .addToBackStack(null)
+                .commit();
+    }
 
 }
 
